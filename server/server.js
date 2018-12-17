dfc8aeec1b422826b159f37636b1d4125a50dfbb
rudolphwen <rudolph.weng@gmail.com>
2018-12-17 13:34:15 +0800

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -132,6 +132,24 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    let user = new User(body);
+
+    // User.findByToken     => model methods are called on a User object
+    // user.generateAuthToken   => instnace mentods are called on an individual user
+
+    user.save().then(() => {
+        // res.send(user);
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
